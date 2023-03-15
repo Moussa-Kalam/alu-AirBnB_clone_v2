cedaed54b3aaadb76a313a1353735bf7f27be810
Moussa-Kalam <m.amzat@alustudent.com>
2023-03-15 03:27:39 +0200

    fix: flask app start

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-""" Web application listening on 0.0.0.0, port 5000"""
+""" Web application listening on 0.0.0.0, port 5000 """
 from flask import Flask
 
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
+@app.route("/")
 def hello_hbnb():
     """ Display Hello HBNB! """
     return "Hello HBNB!"
@@ -13,3 +13,4 @@ def hello_hbnb():
 
 if __name__ == "main":
     app.run(host='0.0.0.0', port=5000)
+    app.url_map.strict_slashes = False
